9f0e903650a5d582427f60f0c200817d7456a048
Will Lapinel <whlapinel@gmail.com>
2024-02-12 13:57:08 -0500

    create components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import MenuComponent from './menu-component/menu-component';
+import FooterComponent from './footer-component/footer-component';
+import AboutComponent from './about-component/about-component';
+import LoginComponent from './login-component/login-component';
+import HomepageComponent from './homepage-component/homepage-component';
 
-function App() {
+import './App.css';
+import HeroComponent from './hero-component/hero-component';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MenuComponent />
+      <HeroComponent/>
+      <HomepageComponent />
+      <FooterComponent />
     </div>
   );
 }
 
-export default App;
