7b257ee27b47df7bfce27eeaf17e14e752eaafe2
Will Lapinel <whlapinel@gmail.com>
2024-02-15 19:39:08 -0500

    add data fetching and server

---
+++ server/server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/hello', (req, res) => {
+    res.send('Hello!');
+});
+
+app.get('/budget', (req, res) => {
+    console.log('Request for budget data');
+    res.json({
+        myBudget: [
+            {
+                title: 'Eat Out',
+                budget: 25
+            },
+            {
+                title: 'Rent',
+                budget: 375
+            },
+            {
+                title: 'Grocery',
+                budget: 110
+            },
+            {
+                title: 'Utilities',
+                budget: 150
+            },
+            {
+                title: 'Transportation',
+                budget: 50
+            },
+            {
+                title: 'Entertainment',
+                budget: 100
+            }
+        ]
+    });
+});
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+});
--- src/pie-chart-component/pie-chart-component.js
+++ src/pie-chart-component/pie-chart-component.js
@@ -2,15 +2,47 @@
 
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+}
+
+
+export default function PieChartComponent() {
+  const [data, setData] = useState({
+    labels: [],
     datasets: [
       {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: 'budget',
+        data: [],
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
@@ -29,16 +61,53 @@ const data = {
         ],
         borderWidth: 1,
       },
-    ],  
-  }
-  
+    ],
+  });
 
-export default function PieChartComponent() {
+  useEffect(() => {
+    async function getChartData() {
+      const url = 'http://localhost:3001/budget';
+      const res = await axios.get(url);
+      console.log(res.data.myBudget);
+      const budgetData = res.data.myBudget;
+      const titles = budgetData.map((item) => item.title);
+      const budgets = budgetData.map((item) => item.budget);
+      const updatedData = {
+        labels: titles,
+        datasets: [
+          {
+            label: '# of Votes',
+            data: budgets,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      }
+      setData(updatedData);
+    }
+    getChartData()
+  }, []);
 
-    // FIXME: make chart smaller
-    return (
-        <div className='pie-chart'>
-            <Pie data={data} options={{responsive: true}}/>
-        </div>
-    )
+
+  // FIXME: make chart smaller
+  return (
+    <div className='pie-chart'>
+      <Pie data={data} options={{ responsive: true }} />
+    </div>
+  )
 }
